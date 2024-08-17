2ccc031e00a701bbf2d6d2831c60f9458779373b
Saleem Ahmed <crazysal.14@gmail.com>
2024-08-16 23:33:06 -0700

    Update model.py

--- model.py
+++ model.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3.12
 # -*- coding: utf-8 -*-
+"""
+Created on July, 2018
+Updated on August, 2024
+
+@author: Tangrizzly
+"""
 
 import datetime
 import math
@@ -55,4 +61,92 @@ class SessionGraph(Module):
         self.n_node = n_node
         self.batch_size = opt.batchSize
         self.nonhybrid = opt.nonhybrid
-        self.embedding =
+        self.embedding = nn.Embedding(self.n_node, self.hidden_size)
+        self.gnn = GNN(self.hidden_size, step=opt.step)
+        self.linear_one = nn.Linear(self.hidden_size, self.hidden_size, bias=True)
+        self.linear_two = nn.Linear(self.hidden_size, self.hidden_size, bias=True)
+        self.linear_three = nn.Linear(self.hidden_size, 1, bias=False)
+        self.linear_transform = nn.Linear(self.hidden_size * 2, self.hidden_size, bias=True)
+        self.loss_function = nn.CrossEntropyLoss()
+        self.optimizer = torch.optim.Adam(self.parameters(), lr=opt.lr, weight_decay=opt.l2)
+        self.scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer, step_size=opt.lr_dc_step, gamma=opt.lr_dc)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        stdv = 1.0 / math.sqrt(self.hidden_size)
+        for weight in self.parameters():
+            weight.data.uniform_(-stdv, stdv)
+
+    def compute_scores(self, hidden, mask):
+        ht = hidden[torch.arange(mask.shape[0]).long(), torch.sum(mask, 1) - 1]  # batch_size x latent_size
+        q1 = self.linear_one(ht).view(ht.shape[0], 1, ht.shape[1])  # batch_size x 1 x latent_size
+        q2 = self.linear_two(hidden)  # batch_size x seq_length x latent_size
+        alpha = self.linear_three(torch.sigmoid(q1 + q2))
+        a = torch.sum(alpha * hidden * mask.view(mask.shape[0], -1, 1).float(), 1)
+        if not self.nonhybrid:
+            a = self.linear_transform(torch.cat([a, ht], 1))
+        b = self.embedding.weight[1:]  # n_nodes x latent_size
+        scores = torch.matmul(a, b.transpose(1, 0))
+        return scores
+
+    def forward(self, inputs, A):
+        hidden = self.embedding(inputs)
+        hidden = self.gnn(A, hidden)
+        return hidden
+
+
+def trans_to_cuda(variable):
+    return variable.cuda() if torch.cuda.is_available() else variable
+
+
+def trans_to_cpu(variable):
+    return variable.cpu() if torch.cuda.is_available() else variable
+
+
+def forward(model, i, data):
+    alias_inputs, A, items, mask, targets = data.get_slice(i)
+    alias_inputs = trans_to_cuda(torch.Tensor(alias_inputs).long())
+    items = trans_to_cuda(torch.Tensor(items).long())
+    A = trans_to_cuda(torch.Tensor(A).float())
+    mask = trans_to_cuda(torch.Tensor(mask).long())
+    hidden = model(items, A)
+    get = lambda i: hidden[i][alias_inputs[i]]
+    seq_hidden = torch.stack([get(i) for i in torch.arange(len(alias_inputs)).long()])
+    return targets, model.compute_scores(seq_hidden, mask)
+
+
+def train_test(model, train_data, test_data):
+    model.scheduler.step()
+    print('Start training: ', datetime.datetime.now())
+    model.train()
+    total_loss = 0.0
+    slices = train_data.generate_batch(model.batch_size)
+    for i, j in zip(slices, np.arange(len(slices))):
+        model.optimizer.zero_grad()
+        targets, scores = forward(model, i, train_data)
+        targets = trans_to_cuda(torch.Tensor(targets).long())
+        loss = model.loss_function(scores, targets - 1)
+        loss.backward()
+        model.optimizer.step()
+        total_loss += loss
+        if j % int(len(slices) / 5 + 1) == 0:
+            print('[%d/%d] Loss: %.4f' % (j, len(slices), loss.item()))
+    print('\tLoss:\t%.3f' % total_loss)
+
+    print('Start predicting: ', datetime.datetime.now())
+    model.eval()
+    hit, mrr = [], []
+    slices = test_data.generate_batch(model.batch_size)
+    for i in slices:
+        targets, scores = forward(model, i, test_data)
+        sub_scores = scores.topk(20)[1]
+        sub_scores = trans_to_cpu(sub_scores).detach().numpy()
+        for score, target, mask in zip(sub_scores, targets, test_data.mask):
+            hit.append(np.isin(target - 1, score))
+            if len(np.where(score == target - 1)[0]) == 0:
+                mrr.append(0)
+            else:
+                mrr.append(1 / (np.where(score == target - 1)[0][0] + 1))
+    hit = np.mean(hit) * 100
+    mrr = np.mean(mrr) * 100
+    return hit, mrr
